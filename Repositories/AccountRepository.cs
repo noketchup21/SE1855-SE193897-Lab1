c9bcda98238a56acfb1be790c3969b24496c19bc
noketchup21 <luongnguyenduykhang2782005@gmail.com>
2025-06-05 19:10:16 +0700

    Add project files.

---
+++ Repositories/AccountRepository.cs
@@ -0,0 +1,10 @@
+﻿using BusinessObjects;
+using DataAccessLayer;
+
+namespace Repositories
+{
+    public class AccountRepository : IAccountRepository
+    {
+        public AccountMember GetAccountById(string accountID) => AccountDAO.GetAccountById(accountID);
+    }
+}
---
+++ Services/CategoryService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObjects;
+using Repositories;
+
+namespace Services
+{
+    public class CategoryService : ICatergoryService
+    {
+        private readonly ICatergoryRepository icategoryRepository;
+        public CategoryService()
+        {
+            icategoryRepository = new CategoryRepository();
+        }
+        public List<Category> GetCategories()
+        {
+            return icategoryRepository.GetCategories();
+        }
+    }
+}
